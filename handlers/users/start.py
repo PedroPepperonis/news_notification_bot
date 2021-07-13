26fbb2e72367f57a1286cf21535e8c3b4ee3cacd
Pedro <theskorpions2014@gmail.com>
2021-07-13 15:21:20 +0300

    First commit

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,10 +1,9 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
-from loader import dp, bot
+from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f'Дарова, {message.from_user.first_name}')
-    await bot.send_message(f'Дарова, {message.from_user.first_name}')

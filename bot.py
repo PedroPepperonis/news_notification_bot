2689fcda9afa812a8267017fccb31933520f5405
Pedro <theskorpions2014@gmail.com>
2021-07-28 23:08:04 +0300

    Webhooks

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import asyncio
 
-from aiogram import executor
+from aiogram.utils.executor import start_webhook
 
 import handlers
 from handlers.users.send_news import bot_send
 from loader import dp, bot
+from config.config import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 
 
 async def on_startup(dispatcher):
@@ -18,13 +19,12 @@ async def on_shutdown(dispatcher):
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.create_task(bot_send(10))
-    # start_webhook(
-    #     dispatcher=dp,
-    #     webhook_path=WEBHOOK_PATH,
-    #     on_startup=on_startup,
-    #     on_shutdown=on_shutdown,
-    #     skip_updates=True,
-    #     host=WEBAPP_HOST,
-    #     port=WEBAPP_PORT,
-    # )
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )

46ec6ae0393b4851b635e79ee82ce4cf8bbf4503
diemondss <45741067+diemondss@users.noreply.github.com>
2021-08-04 22:30:48 +0300

    webhooks add

--- config/config.py
+++ config/config.py
@@ -6,23 +6,23 @@ HEADERS = {
 }
 
 
-BOT_TOKEN = '1894140705:AAESWyMLMoEV323l3ffaVlitvDveBFUq1fo'
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 if not BOT_TOKEN:
     print('You have forgot to set BOT_TOKEN')
     quit()
 
-DATABASE_URL = 'postgres://ptllmijpzbypbo:0406fb9c8a60e668b8b760e8d0bd658dac098ca5d8cf4a6907c04716a9c0fa3b@ec2-54-220-35-19.eu-west-1.compute.amazonaws.com:5432/d259vgcgajo41l'
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-# HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-# # webhook settings
-# WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-# WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
-# WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-# # webserver settings
-# WEBAPP_HOST = '0.0.0.0'
-# WEBAPP_PORT = int(os.getenv('PORT'))
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv('PORT'))
 
 URL_STOPGAME = 'https://stopgame.ru'
 URL_IGROMANIA = 'https://www.igromania.ru'
